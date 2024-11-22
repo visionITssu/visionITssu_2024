5c4a7d868cc4f3b61683705abedc2e6d23ddecd8
intj=0 <ebyeon147@soongsil.ac.kr>
2024-11-22 14:28:38 +0900

    [#30] verification swagger 코드 추가

--- apps/server/src/photo-verification/photo-verification.controller.ts
+++ apps/server/src/photo-verification/photo-verification.controller.ts
@@ -2,13 +2,28 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { VerificationService } from "./photo-verification.service";
-
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiCreatedResponse,
+} from "@nestjs/swagger";
+import { image } from "@tensorflow/tfjs";
 
 @Controller("verification")
+@ApiTags("사진 검증 API")
 export class VerificationController {
   constructor(private readonly verificationService: VerificationService) {}
 
   @Post()
+  @ApiOperation({
+    summary: " 사진 검증 API",
+    description: "yolo 모델을 통해 사진을 검증한다.",
+  })
+  @ApiCreatedResponse({
+    description: "yolo 모델을 통해 사진을 검증한다.",
+    type: File,
+  })
   @UseInterceptors(FileInterceptor("image"))
   async handleImageUpload(@UploadedFile() file: Express.Multer.File) {
     return this.verificationService.getVerification(file);
