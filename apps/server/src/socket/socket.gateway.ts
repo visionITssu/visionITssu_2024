d162c551281b65d5f5aea2bd26b21a37e4b9dc74
intj=0 <ebyeon147@soongsil.ac.kr>
2024-11-29 23:44:19 +0900

    [#57] hotfix: 모델 결과 전송 중, 모델 결과 및 표정 조건문 수정

--- apps/server/src/socket/socket.gateway.ts
+++ apps/server/src/socket/socket.gateway.ts
@@ -32,8 +32,8 @@ export class SocketGateway {
 
   async loadModelFromEC2(input: string): Promise<any> {
     try {
-      //const response = await axios.post("http://13.125.14.183:5001/process", {
-      const response = await axios.post("http://localhost:5001/process", {
+      const response = await axios.post("http://13.125.14.183:5001/process", {
+        //const response = await axios.post("http://localhost:5001/process", {
         input: input, // Base64 인코딩된 이미지 데이터
       });
       return response.data; // Python에서 반환된 JSON 데이터를 반환
@@ -58,7 +58,7 @@ export class SocketGateway {
       if (!yolo || yolo.length === 0) {
         tempVerificationResult[0] = 1; // yolo가 비어있을 경우 1
       } else {
-        tempVerificationResult[0] = 1; // yolo에 값이 있으면 1
+        tempVerificationResult[0] = 0; // yolo에 값이 있으면 1
       }
 
       // 조건 2: 얼굴 밝기와 눈썹
@@ -80,7 +80,8 @@ export class SocketGateway {
       // 조건 4: 표정 확인
       if (
         facePredictions.valid_mouth_openness === 1 &&
-        facePredictions.valid_mouth_smile === 1
+        facePredictions.valid_mouth_smile === 1 &&
+        facePredictions.valid_eye_openness === 1
       ) {
         tempVerificationResult[3] = 1;
       }
